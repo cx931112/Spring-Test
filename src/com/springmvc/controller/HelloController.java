df30826eae3dd1a510e12354d98038513bb6b8b1
cx931112 <1140885639@qq.com>
2016-11-29 17:49:37 +0800

    value的值说明

--- src/com/springmvc/controller/HelloController.java
+++ src/com/springmvc/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	
+	//value的值不需要在前面加“/”
 @RequestMapping(value="home")
 public String index(){
 	System.out.println("ok");
