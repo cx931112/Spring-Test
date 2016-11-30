ba35551ea2ef6ce32fbe20a2d2ba723ee901d554
cx931112 <1140885639@qq.com>
2016-11-30 17:58:02 +0800

    对原有的补充01

--- src/com/springmvc/controller/HelloController.java
+++ src/com/springmvc/controller/HelloController.java
@@ -2,13 +2,20 @@ package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	
 @RequestMapping(value="/home")
-public String index(){
+/*  可以返回String类型
+    public String index(){
 	System.out.println("ok");
 	return "index";
+}*/
+public ModelAndView index(){
+	ModelAndView mv=new ModelAndView();
+	mv.setViewName("index");
+	return mv;
 }
 }
