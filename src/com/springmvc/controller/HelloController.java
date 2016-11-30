977cfbc29de741ad39527a6b7483e7c32a2c6c69
cx931112 <1140885639@qq.com>
2016-11-30 14:59:11 +0800

    value值说明更改

--- src/com/springmvc/controller/HelloController.java
+++ src/com/springmvc/controller/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	//value的值不需要在前面加“/”
-@RequestMapping(value="home")
+	
+@RequestMapping(value="/home")
 public String index(){
 	System.out.println("ok");
 	return "index";
